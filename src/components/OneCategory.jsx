5a10ae03f485b07a48f1929b7bfbcbcf69fd189f
Paulonbass75 <paulmpool@yahoo.com>
2023-06-28 15:04:00 -0400

    OneCategory fetch

--- src/components/OneCategory.jsx
+++ src/components/OneCategory.jsx
@@ -5,10 +5,10 @@ import { Link, useParams, useLocation } from "react-router-dom";
 export default function OneCategory() {
 // get prop passed from Categories.jsx
 const location = useLocation();
-const { genreName } = location.state;
+const { categoryName } = location.state;
 
 // set stateful variable for movies
-const [movies, setMovies] = useState([]);
+const [categories, setCategories] = useState([]);
 
 // get id from url
 let { id } = useParams();
@@ -23,51 +23,49 @@ const requestOptions = {
     headers: headers,
 };
 
-fetch(`/movies/genres/${id}`, requestOptions)
-    .then((response) => response.json())
-    .then((data) => {
-        if (data.error) {
-            console.log(data.message);
-        }else {
-    setMovies(data);
-        }
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+fetch(`http://10.0.1.244/api/v1/categories`, requestOptions)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.error) {
+      console.log(data.message);
+    } else {
+      setCategories(data);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }, [id]);
 
-// render movies
+// render category
 
 
 
 
   return (
     <>
-      <h2>Categories {genreName}</h2>
+      <h2>Category {categoryName}</h2>
         <hr />
-        {movies ? (
+        {categories ? (
       <table className="table tables-triped table-hover">
         <thead>
             <tr>
-                <th>Movie</th>
-                <th>Release Date</th>
-                <th>Rating</th>
+                
             </tr>
         </thead>
         <tbody>
-            {movies.map((m) => (
-                <tr key={m.id}>
+            {categories.map((c) => (
+                <tr key={c.id}>
                     <td>
-                        <Link to={`/movies/${m.id}`}>{m.title}</Link>
+                        <Link to={`/categories/${c.id}`}>{c.name}</Link>
                     </td>
-                    <td>{m.release_date}</td>
-                    <td>{m.mpaa_rating}</td>
+                    <td>{c.name}</td>
+                    <td>{c.head_title}</td>
                 </tr>
             ))}
         </tbody>
       </table>
-      ):( <p>No movies found</p>
+      ):( <p>Category Not Found</p>
       
       )}
     </>
