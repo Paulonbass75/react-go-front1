443c6351f52218f0fe82c98b7a155d6b6af27b42
Paulonbass75 <paulmpool@yahoo.com>
2023-06-29 16:03:43 -0400

    one category

--- src/components/OneCategory.jsx
+++ src/components/OneCategory.jsx
@@ -3,70 +3,62 @@ import { useEffect, useState } from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
 
 export default function OneCategory() {
-// get prop passed from Categories.jsx
-const location = useLocation();
-const { categoryName } = location.state;
+  // get prop passed from Categories.jsx
+  const location = useLocation();
+//   const { categoryName } = location.state;
 
-// set stateful variable for movies
-const [categories, setCategories] = useState([]);
+  // set stateful variable for categories
+  const [categories, setCategories] = useState([]);
 
-// get id from url
-let { id } = useParams();
+  // get id from url
+  let { id } = useParams();
 
-// use useEffect to fetch movies for this category
-useEffect(() => {
+  // use useEffect to fetch categories for this category
+  useEffect(() => {
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
 
-const requestOptions = {
-    method: "GET",
-    headers: headers,
-};
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    };
 
-fetch(`http://10.0.1.244/api/v1/categories`, requestOptions)
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.error) {
-      console.log(data.message);
-    } else {
-      setCategories(data);
-    }
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-}, [id]);
+    fetch(`http://10.0.1.244/api/v1/categories`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          console.log(data.message);
+        } else {
+          setCategories(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
 
-// render category
-
-
-
+  // render category
 
   return (
     <>
-      <h2>Category {categoryName}</h2>
-        <hr />
-        {categories ? (
-      <table className="table tables-triped table-hover">
-        <thead>
-            <tr>
-                
-            </tr>
-        </thead>
-        <tbody>
-            {categories.map((c) => (
-                <tr key={c.id}>
-                    <td>
-                        <Link to={`/categories/${c.id}`}>{c.name}</Link>
-                    </td>
-                    <td>{c.name}</td>
-                    <td>{c.head_title}</td>
-                </tr>
-            ))}
-        </tbody>
-      </table>
-      ):( <p>Category Not Found</p>
-      
+      {/* <h2>Category {categoryName}</h2> */}
+      <hr />
+      {categories ? (
+        <ul className="table tables-triped table-hover">
+          {categories.map((c) => (
+            <li key={c.id}>
+              <a href={`/c/${c.parent_id} &{c.id}`}>
+                {/* <img src={c.img} alt={c.name} /> */}
+                <h3 className="font-bold">
+                  ${c.name} ${c.parent_name}
+                </h3>
+                <p>{c.head_desc}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Category Not Found</p>
       )}
     </>
   );
