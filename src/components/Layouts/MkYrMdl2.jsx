e4ccc59a390051c5669e7d76d3760dcbbbc5ec3c
Paulonbass75 <paulmpool@yahoo.com>
2023-07-14 15:42:49 -0400

    Moving code

--- src/components/Layouts/MkYrMdl2.jsx
+++ src/components/Layouts/MkYrMdl2.jsx
@@ -33,8 +33,56 @@ export default function MyComponent() {
 
   // methods
   const filterChg = () => {
-    // ...
-    // Rest of the code for this method
+    if(this.selMake == 'null')
+            {
+                this.selMake = null
+            }
+            if(this.selModel == 'null')
+            {
+                this.selModel = null
+            }
+            if(this.selYear == 'null')
+            {
+                this.selYear = null
+            }
+            if(this.selYear && !this.selMake)
+            {
+                axios.get('/api/v2/filter?year='+this.selYear)
+                .then((response) => {
+                    this.years = response.data.years
+                    this.makes = response.data.makes
+                    this.selModel = null
+                })
+            }
+            if(!this.selYear && this.selMake)
+            {
+                axios.get('/api/v2/filter?make='+this.selMake)
+                .then((response) => {
+                    this.years = response.data.years
+                    this.models = response.data.models
+                    this.selModel = null
+                })
+            }
+            if(this.selYear && this.selMake)
+            {
+                axios.get('/api/v2/filter?year='+this.selYear+'&make='+this.selMake)
+                .then((response) => {
+                    this.models = response.data.models
+                    this.makes = response.data.makes
+                    this.years = response.data.years
+                    this.selModel = null
+                    if(response.data.makeclr)
+                    {
+                        this.selMake = null
+                        this.products = []
+                    }
+                    if(response.data.yearclr)
+                    {
+                        this.selYear = null
+                        this.products = []
+                    }
+                })
+            }
   }
 
   const modelChg = () => {
