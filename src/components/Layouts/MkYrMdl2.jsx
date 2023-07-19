69cc7ca3adff9090555f8a632bbed432d5ffd899
Paulonbass75 <paulmpool@yahoo.com>
2023-07-19 09:19:09 -0400

    fixed

--- src/components/Layouts/MkYrMdl2.jsx
+++ src/components/Layouts/MkYrMdl2.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function MkYrMdl2() {
   // Initial setup
@@ -12,7 +12,7 @@ export default function MkYrMdl2() {
     selYear: null,
     selMake: null,
     selModel: null,
-  })
+  });
 
   // On component did mount equivalent
   useEffect(() => {
@@ -31,196 +31,196 @@ export default function MkYrMdl2() {
         categories: response.data,
       }));
     });
-  }, [])
+  }, []);
 
   // methods
   const filterChg = () => {
-    if(this.selMake === 'null')
-            {
-                this.selMake = null
-            }
-            if(this.selModel === 'null')
-            {
-                this.selModel = null
-            }
-            if(this.selYear === 'null')
-            {
-                this.selYear = null
-            }
-            if(this.selYear && !this.selMake)
-            {
-                axios
-                  .get("http://10.0.1.244/api/v2/filter?year=" + this.selYear)
-                  .then((response) => {
-                    this.years = response.data.years;
-                    this.makes = response.data.makes;
-                    this.selModel = null;
-                  });
-            }
-            if(!this.selYear && this.selMake)
-            {
-                axios
-                  .get("http://10.0.1.244/api/v2/filter?make=" + this.selMake)
-                  .then((response) => {
-                    this.years = response.data.years;
-                    this.models = response.data.models;
-                    this.selModel = null;
-                  });
-            }
-            if(this.selYear && this.selMake)
-            {
-                axios
-                  .get(
-                    "http://10.0.1.244/api/v2/filter?year=" +
-                      this.selYear +
-                      "&make=" +
-                      this.selMake
-                  )
-                  .then((response) => {
-                    this.models = response.data.models;
-                    this.makes = response.data.makes;
-                    this.years = response.data.years;
-                    this.selModel = null;
-                    if (response.data.makeclr) {
-                      this.selMake = null;
-                      this.products = [];
-                    }
-                    if (response.data.yearclr) {
-                      this.selYear = null;
-                      this.products = [];
-                    }
-                    console.log(response.data);
-                  });
-            }
-  }
+    if (this.selMake === "null") {
+      this.selMake = null;
+    }
+    if (this.selModel === "null") {
+      this.selModel = null;
+    }
+    if (this.selYear === "null") {
+      this.selYear = null;
+    }
+    if (this.selYear && !this.selMake) {
+      axios
+        .get("http://10.0.1.244/api/v2/filter?year=" + this.selYear)
+        .then((response) => {
+          this.years = response.data.years;
+          this.makes = response.data.makes;
+          this.selModel = null;
+        });
+    }
+    if (!this.selYear && this.selMake) {
+      axios
+        .get("http://10.0.1.244/api/v2/filter?make=" + this.selMake)
+        .then((response) => {
+          this.years = response.data.years;
+          this.models = response.data.models;
+          this.selModel = null;
+        });
+    }
+    if (this.selYear && this.selMake) {
+      axios
+        .get(
+          "http://10.0.1.244/api/v2/filter?year=" +
+            this.selYear +
+            "&make=" +
+            this.selMake
+        )
+        .then((response) => {
+          this.models = response.data.models;
+          this.makes = response.data.makes;
+          this.years = response.data.years;
+          this.selModel = null;
+          if (response.data.makeclr) {
+            this.selMake = null;
+            this.products = [];
+          }
+          if (response.data.yearclr) {
+            this.selYear = null;
+            this.products = [];
+          }
+          console.log(response.data);
+        });
+    }
+  };
 
   const modelChg = () => {
-     if(this.selModel !== 'null' && this.selMake && (this.selYear || this.selYear !== 'null'))
-            {
-                axios
-                  .get(
-                    "http://10.0.1.244/api/v2/categories?model=" +
-                      this.selModel +
-                      "&year=" +
-                      this.selYear +
-                      "&make=" +
-                      this.selMake
-                  )
-                  .then((response) => {
-                    this.categories = response.data;
-                    this.products = [];
-                  });
-                 
-            }
-            if(this.selModel !== 'null' && this.selMake && (!this.selYear || this.selYear === 'null'))
-            {
-                console.log('1')
-            }
-  }
+    if (
+      this.selModel !== "null" &&
+      this.selMake &&
+      (this.selYear || this.selYear !== "null")
+    ) {
+      axios
+        .get(
+          "http://10.0.1.244/api/v2/categories?model=" +
+            this.selModel +
+            "&year=" +
+            this.selYear +
+            "&make=" +
+            this.selMake
+        )
+        .then((response) => {
+          this.categories = response.data;
+          this.products = [];
+        });
+    }
+    if (
+      this.selModel !== "null" &&
+      this.selMake &&
+      (!this.selYear || this.selYear === "null")
+    ) {
+      console.log("1");
+    }
+  };
 
   const catSel = (catId) => {
-   if(!this.selModel)
-            {
-                this.selMake = null
-                this.selYear = null
-                axios
-                  .get("http://10.0.1.244/api/v2/categories/" + catId)
-                  .then((response) => {
-                    this.categories = response.data.categories;
-                    this.products = response.data.products;
-                  });
-            }
-            if(this.selModel)
-            {
-                axios
-                  .get(
-                    "http://10.0.1.244/api/v2/categories/" +
-                      catId +
-                      "?year=" +
-                      this.selYear +
-                      "&make=" +
-                      this.selMake +
-                      "&model=" +
-                      this.selModel
-                  )
-                  .then((response) => {
-                    this.categories = response.data.categories;
-                    this.products = response.data.products;
-                  });
-                console.log(catId)
-            }
-  }
+    if (!this.selModel) {
+      this.selMake = null;
+      this.selYear = null;
+      axios
+        .get("http://10.0.1.244/api/v2/categories/" + catId)
+        .then((response) => {
+          this.categories = response.data.categories;
+          this.products = response.data.products;
+        });
+    }
+    if (this.selModel) {
+      axios
+        .get(
+          "http://10.0.1.244/api/v2/categories/" +
+            catId +
+            "?year=" +
+            this.selYear +
+            "&make=" +
+            this.selMake +
+            "&model=" +
+            this.selModel
+        )
+        .then((response) => {
+          this.categories = response.data.categories;
+          this.products = response.data.products;
+        });
+      console.log(catId);
+    }
+  };
 
   // ...
   // Rest of the component
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         <div className="col-md-12">
-//           <div className="panel panel-default">
-//             <div className="panel-heading">
-//               <div className="panel-body">
-//                 <div className="form-group">
-//                   <label className="col-sm-2 control-label">Year</label>
-//                   <div className="col-sm-10">
-//                     <select
-//                       className="form-control"
-//                       id="selYear"
-//                       onChange={(e) =>
-//                         setData({ ...data, selYear: e.target.value })
-//                       }
-//                     >
-//                       <option value="null">Select Year</option>
-//                       {Object.keys(year).map((key, i) => (
-//                         <option key={year[i].year} value={year[i].year}>
-//                           {year[i].year}
-//                         </option>
-//                       ))}
-//                     </select>
-//                   </div>
-//                 </div>
-//                 <div className="form-group">
-//                   <label className="col-sm-2 control-label">Make</label>
-//                   <div className="col-sm-10">
-//                     <select
-//                       className="form-control"
-//                       id="selMake"
-//                       onChange={(e) =>
-//                         setData({ ...data, selMake: e.target.value })
-//                       }
-//                     >
-//                       {/* <option value="null">Select Make</option>
-//             {data.makes.map((make) => (
-//               <option key={make} value={make}>{make}</option>
-//             ))} */}
-//                     </select>
-//                   </div>
-//                 </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <div className="panel-body">
+                <div className="form-group">
+                  <label className="col-sm-2 control-label">Year</label>
+                  <div className="col-sm-10">
+                    <select
+                      className="form-control"
+                      id="selYear"
+                      onChange={(e) =>
+                        setData({ ...data, selYear: e.target.value })
+                      }
+                    >
+                      <option value="null">Select Year</option>
+                      {Object.keys(data.years).map((key, i) => (
+                        <option
+                          key={data.years[i]}
+                          value={data.years[i]}
+                        >
+                          {data.years[i]}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-2 control-label">Make</label>
+                  <div className="col-sm-10">
+                    <select
+                      className="form-control"
+                      id="selMake"
+                      onChange={(e) =>
+                        setData({ ...data, selMake: e.target.value })
+                      }
+                    >
+                      <option value="null">Select Make</option>
+                      {Object.keys(data.makes).map((key, index) => (
+                        <option key={key} value={data.makes[index].make}>
+                          {data.makes[index].make}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
 
-//                 <div className="form-group">
-//                   <label className="col-sm-2 control-label">Model</label>
-//                   <div className="col-sm-10">
-//                     <select
-//                       className="form-control"
-//                       id="selModel"
-//                       onChange={(e) =>
-//                         setData({ ...data, selModel: e.target.value })
-//                       }
-//                     >
-//                       {/* <option value="null">Select Model</option>
-//             {data.models.map((model) => (
-//               <option key={model} value={model}>{model}</option>
-//             ))} */}
-//                     </select>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
+                <div className="form-group">
+                  <label className="col-sm-2 control-label">Model</label>
+                  <div className="col-sm-10">
+                    <select
+                      className="form-control"
+                      id="selModel"
+                      onChange={(e) =>
+                        setData({ ...data, selModel: e.target.value })
+                      }
+                    >
+                      {/* <option value="null">Select Model</option>
+            {data.models.map((model) => (
+              <option key={model} value={model}>{model}</option>
+            ))} */}
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
---
+++ src/components/MmyFilter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MmyFilter() {
+  return (
+    <div>MmyFilter</div>
+  )
+}
